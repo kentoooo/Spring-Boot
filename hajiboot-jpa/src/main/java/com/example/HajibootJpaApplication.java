e6b319f5b38a61d5cc2b549b178e336df2587d00
岡村謙杜 <okamurakento@okamurakenmori-no-MacBook-Pro.local>
2017-10-03 00:27:54 +0900

    jpa終了

--- hajiboot-jpa/src/main/java/com/example/HajibootJpaApplication.java
+++ hajiboot-jpa/src/main/java/com/example/HajibootJpaApplication.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.example.domain.Customer;
 import com.example.repository.CustomerRepository;
@@ -18,7 +21,16 @@ public class HajibootJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// データ追加
-		Customer created =customerRepository.save(new Custom);
+		Customer created =customerRepository.save(new Customer(null,"Nobi","Nobita"));
+		System.out.println(created + " is created!");
+		// ページング処理
+		Pageable pageable = new PageRequest(0,3);
+		Page<Customer> page = customerRepository.findAll(pageable);
+		System.out.println("1ページのデータ数=" + page.getSize());
+		System.out.println("現在のページ=" + page.getNumber());
+		System.out.println("全ページ数=" + page.getTotalPages());
+		System.out.println("全データ数=" + page.getTotalElements());
+		page.getContent().forEach(System.out::println);
 	}
 
 	public static void main(String[] args) {
